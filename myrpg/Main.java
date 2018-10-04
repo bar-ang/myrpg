6380e675ee7ee493691f330d376dd4e626f7b912
bar-lb <bar@lightbitslabs.com>
2018-10-05 01:36:27 +0300

    Main function
    Signed-off-by: bar-lb <bar@lightbitslabs.com>

---
+++ myrpg/Main.java
@@ -0,0 +1,10 @@
+package myrpg;
+
+public class Main{
+    public static void main(String[] args){
+        Player player = new Player();
+        
+        System.out.println("hello " + player.character.name);
+
+    }
+}
